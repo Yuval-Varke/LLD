f77fdda4bd4c3b24d1d6a8c8bab1e960de9223c4
Yuval Varke <varke.vishu01@gmail.com>
2026-09-25 08:19:42 +0530

    Memento Pattern for Text Editor

---
+++ 3. Behavioural Design Patterns/a. Memento Pattern/history.py
@@ -0,0 +1,18 @@
+from text_memento import TextMemento
+from typing import List
+
+class History:
+    def __init__(self):
+        self.__history:List[TextMemento] = []
+
+    def save_state(self,tm:TextMemento):
+        self.__history.append(tm)
+
+    def undo(self) -> TextMemento:
+        if len(self.__history) > 0:
+            self.__history.pop()
+            if len(self.__history) == 0:
+                return TextMemento("")
+            return self.__history[-1]
+        else:
+            return TextMemento("")
